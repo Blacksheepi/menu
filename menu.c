eb80cb4b618c3bd5dd8aa623b8fb202ee0f21fb1
Blacksheepi <douyiming2011@163.com>
2016-11-13 12:06:14 +0800

    Update menu.c

--- menu.c
+++ menu.c
@@ -69,9 +69,9 @@ void Love()
     for (y = 1.5f;y > -1.5f;y -= 0.1f)
     {
         for (x = -1.5f;x < 1.5f;x += 0.05f)
-	    {
-	        a = x*x + y*y - 1;
-	        putchar(a*a*a - x*x*y*y*y <= 0.0f ? '*' : ' ');
+	{
+	    a = x*x + y*y - 1;
+	    putchar(a*a*a - x*x*y*y*y <= 0.0f ? '*' : ' ');
         }
 	putchar('\n'); 
     }
